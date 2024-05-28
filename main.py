88e34d9f4c2d86f99894638ead459905b0bfac53
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-05-28 11:06:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import base64
 # Set page configuration
 st.set_page_config(layout="wide")
 
+AUTHORIZED_USERS = st.secrets["authorized_users"]
+
 # Define file paths
 file_paths = ['files/Concord', 'files/Winston', 'files/Lake', 'files/Hickory']
 
@@ -72,121 +74,125 @@ def load_data(file_paths):
             st.error(f"File {file} not found in the repository.")
     return data_frames
 
-# Load data
-data_frames = load_data(file_paths)
-
-# Combine data
-if data_frames:
-    combined_data = pd.concat([df[0] for df in data_frames], ignore_index=True)
-    combined_data.reset_index(drop=True, inplace=True)
-else:
-    combined_data = pd.DataFrame()
-
-# Custom CSS for padding and container width
-st.write(
-    """
-    <style>
-    .main .block-container {
-        padding-top: 1rem;
-        padding-left: 2rem;
-        padding-right: 2rem;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Create tabs
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Concord", "Winston", "Lake", "Hickory", "All Stores"])
-
-# Function to save edited data back to GitHub
-def save_to_github(file_path, data_frame, token):
-    html_data = base64.b64encode(data_frame.to_html(index=False).encode('utf-8')).decode('utf-8')
-    headers = {
-        "Authorization": f"token {token}",
-        "Content-Type": "application/json"
-    }
-    path = file_path
-    url = f"https://api.github.com/repos/cobbtrades/Inventory/contents/{path}"
+user_email = st.experimental_get_query_params().get("email", [""])[0]
+if user_email in AUTHORIZED_USERS:
+    # Load data
+    data_frames = load_data(file_paths)
     
-    # Get the SHA of the file to update
-    response = requests.get(url, headers=headers)
-    response_data = response.json()
-    sha = response_data.get("sha")
-
-    # Prepare the payload
-    payload = {
-        "message": "Update data",
-        "content": html_data,
-        "sha": sha
-    }
-    
-    # Update the file on GitHub
-    update_response = requests.put(url, headers=headers, json=payload)
-    if update_response.status_code == 200:
-        st.success(f"Successfully updated {file_path} on GitHub.")
+    # Combine data
+    if data_frames:
+        combined_data = pd.concat([df[0] for df in data_frames], ignore_index=True)
+        combined_data.reset_index(drop=True, inplace=True)
     else:
-        st.error(f"Failed to update {file_path} on GitHub: {update_response.text}")
-
-# Function to filter data based on selectbox inputs
-def filter_data(df, model, trim, package, color):
-    if model != 'All':
-        df = df[df['MDL'] == model]
-    if trim != 'All':
-        df = df[df['TRIM'] == trim]
-    if package != 'All':
-        df = df[df['PACKAGE'].str.contains(package)]
-    if color != 'All':
-        df = df[df['EXT'] == color]
-    return df
-
-# Function to display data for each store
-def display_store_data(tab, df, file_path, store_name, tab_key):
-    with tab:
-        cols = st.columns([2, 1, 1, 1, 1])
-        with cols[0]:
-            st.markdown(f"### {store_name} Inventory")
-        with cols[1]:
-            model = st.selectbox('Model', options=['All'] + df['MDL'].unique().tolist(), key=f'{tab_key}_model')
-        with cols[2]:
-            # Filter options based on selected model
-            trims = ['All'] if model == 'All' else ['All'] + df[df['MDL'] == model]['TRIM'].unique().tolist()
-            trim = st.selectbox('Trim', options=trims, key=f'{tab_key}_trim')
-        with cols[3]:
-            packages = ['All'] if model == 'All' else ['All'] + df[df['MDL'] == model]['PACKAGE'].unique().tolist()
-            package = st.selectbox('Package', options=packages, key=f'{tab_key}_package')
-        with cols[4]:
-            colors = ['All'] if model == 'All' else ['All'] + df[df['MDL'] == model]['EXT'].unique().tolist()
-            color = st.selectbox('Color', options=colors, key=f'{tab_key}_color')
-
-        filtered_df = filter_data(df, model, trim, package, color)
+        combined_data = pd.DataFrame()
+    
+    # Custom CSS for padding and container width
+    st.write(
+        """
+        <style>
+        .main .block-container {
+            padding-top: 1rem;
+            padding-left: 2rem;
+            padding-right: 2rem;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    # Create tabs
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Concord", "Winston", "Lake", "Hickory", "All Stores"])
+    
+    # Function to save edited data back to GitHub
+    def save_to_github(file_path, data_frame, token):
+        html_data = base64.b64encode(data_frame.to_html(index=False).encode('utf-8')).decode('utf-8')
+        headers = {
+            "Authorization": f"token {token}",
+            "Content-Type": "application/json"
+        }
+        path = file_path
+        url = f"https://api.github.com/repos/cobbtrades/Inventory/contents/{path}"
         
-        num_rows = len(filtered_df)
-        st.markdown(f"<span style='font-size: small;'>{num_rows} vehicles</span>", unsafe_allow_html=True)
+        # Get the SHA of the file to update
+        response = requests.get(url, headers=headers)
+        response_data = response.json()
+        sha = response_data.get("sha")
+    
+        # Prepare the payload
+        payload = {
+            "message": "Update data",
+            "content": html_data,
+            "sha": sha
+        }
         
-        edited_df = st.data_editor(filtered_df, height=780, hide_index=True, key=f'{tab_key}_data_editor')
-        
-        # Update the original dataframe with the changes from the edited dataframe
-        for index, row in edited_df.iterrows():
-            original_index = df[df['VIN'] == row['VIN']].index[0]
-            df.loc[original_index] = row
-        
-        token = os.getenv('GITHUB_TOKEN')
-        if token:
-            save_to_github(file_path, df, token)
-
-# Display individual store data
-store_names = ["Concord", "Winston", "Lake", "Hickory"]
-tabs = [tab1, tab2, tab3, tab4]
-if data_frames:
-    for i, (df, file_path) in enumerate(data_frames):
-        display_store_data(tabs[i], df, file_path, store_names[i], f'store_{i}')
-
-# Display combined data for all stores
-if not combined_data.empty:
-    num_rows = len(combined_data)
-    with tab5:
-        st.markdown(f"### Group Inventory <span style='font-size: small;'>{num_rows} vehicles</span>", unsafe_allow_html=True)
-        st.data_editor(combined_data, use_container_width=True, height=780, hide_index=True)
+        # Update the file on GitHub
+        update_response = requests.put(url, headers=headers, json=payload)
+        if update_response.status_code == 200:
+            st.success(f"Successfully updated {file_path} on GitHub.")
+        else:
+            st.error(f"Failed to update {file_path} on GitHub: {update_response.text}")
+    
+    # Function to filter data based on selectbox inputs
+    def filter_data(df, model, trim, package, color):
+        if model != 'All':
+            df = df[df['MDL'] == model]
+        if trim != 'All':
+            df = df[df['TRIM'] == trim]
+        if package != 'All':
+            df = df[df['PACKAGE'].str.contains(package)]
+        if color != 'All':
+            df = df[df['EXT'] == color]
+        return df
+    
+    # Function to display data for each store
+    def display_store_data(tab, df, file_path, store_name, tab_key):
+        with tab:
+            cols = st.columns([2, 1, 1, 1, 1])
+            with cols[0]:
+                st.markdown(f"### {store_name} Inventory")
+            with cols[1]:
+                model = st.selectbox('Model', options=['All'] + df['MDL'].unique().tolist(), key=f'{tab_key}_model')
+            with cols[2]:
+                # Filter options based on selected model
+                trims = ['All'] if model == 'All' else ['All'] + df[df['MDL'] == model]['TRIM'].unique().tolist()
+                trim = st.selectbox('Trim', options=trims, key=f'{tab_key}_trim')
+            with cols[3]:
+                packages = ['All'] if model == 'All' else ['All'] + df[df['MDL'] == model]['PACKAGE'].unique().tolist()
+                package = st.selectbox('Package', options=packages, key=f'{tab_key}_package')
+            with cols[4]:
+                colors = ['All'] if model == 'All' else ['All'] + df[df['MDL'] == model]['EXT'].unique().tolist()
+                color = st.selectbox('Color', options=colors, key=f'{tab_key}_color')
+    
+            filtered_df = filter_data(df, model, trim, package, color)
+            
+            num_rows = len(filtered_df)
+            st.markdown(f"<span style='font-size: small;'>{num_rows} vehicles</span>", unsafe_allow_html=True)
+            
+            edited_df = st.data_editor(filtered_df, height=780, hide_index=True, key=f'{tab_key}_data_editor')
+            
+            # Update the original dataframe with the changes from the edited dataframe
+            for index, row in edited_df.iterrows():
+                original_index = df[df['VIN'] == row['VIN']].index[0]
+                df.loc[original_index] = row
+            
+            token = os.getenv('GITHUB_TOKEN')
+            if token:
+                save_to_github(file_path, df, token)
+    
+    # Display individual store data
+    store_names = ["Concord", "Winston", "Lake", "Hickory"]
+    tabs = [tab1, tab2, tab3, tab4]
+    if data_frames:
+        for i, (df, file_path) in enumerate(data_frames):
+            display_store_data(tabs[i], df, file_path, store_names[i], f'store_{i}')
+    
+    # Display combined data for all stores
+    if not combined_data.empty:
+        num_rows = len(combined_data)
+        with tab5:
+            st.markdown(f"### Group Inventory <span style='font-size: small;'>{num_rows} vehicles</span>", unsafe_allow_html=True)
+            st.data_editor(combined_data, use_container_width=True, height=780, hide_index=True)
+    else:
+        st.error("No data to display.")
 else:
-    st.error("No data to display.")
+    st.write("<h1>Not Authorized</h1>", unsafe_allow_html=True)
