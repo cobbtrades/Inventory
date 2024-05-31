f9d287b98213609f8ae14ff66d0a7765bc219090
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-05-31 11:41:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -89,6 +89,7 @@ else:
 def load_incoming_data(file_path):
     if os.path.exists(file_path):
         df = pd.read_excel(file_path, header=4, usecols='B:O')
+        df.rename(columns=new_column_names, inplace=True)
         df['Year'] = df['Year'].astype(str).str.replace(',', '')
         df.fillna('', inplace=True)
         del df['Deal \nNo.']
