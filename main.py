a9aec6db3d4cc89c2ff28ebd920c1387f1b3331c
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-01 09:39:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -402,6 +402,7 @@ with tab3:
 def summarize_incoming_data(df, start_date, end_date):
     df['ETA'] = pd.to_datetime(df['ETA'], errors='coerce')
     filtered_df = df[(df['ETA'] >= start_date) & (df['ETA'] <= end_date)]
+    filtered_df['DEALER_NAME'] = filtered_df['DEALER_NAME'].replace(dealer_acronyms)
     summary = filtered_df.groupby(['DEALER_NAME', 'MDL']).size().reset_index(name='Count')
     pivot_table = pd.pivot_table(summary, values='Count', index='MDL', columns='DEALER_NAME', 
                                  aggfunc=sum, fill_value=0, margins=True, margins_name='Total')
