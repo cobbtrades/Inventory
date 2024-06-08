0f2a470da322fd2a55c65068cf412e9aff43b9e7
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-08 09:31:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -513,28 +513,3 @@ with tab4:
                 st.markdown(dataframe_to_html(balance_to_arrive), unsafe_allow_html=True)
     else:
         st.error("No data to display.")
-# Adding a button to capture the screenshot
-st.markdown(
-    """
-    <button id="capture">Save Tables as Image</button>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-    <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        document.getElementById("capture").onclick = function() {
-            console.log("Button clicked");
-            html2canvas(document.body).then(function(canvas) {
-                console.log("Canvas created");
-                var link = document.createElement('a');
-                link.download = 'tables_screenshot.png';
-                link.href = canvas.toDataURL();
-                link.click();
-                console.log("Download triggered");
-            }).catch(function(error) {
-                console.error("Error capturing screenshot:", error);
-            });
-        };
-    });
-    </script>
-    """,
-    unsafe_allow_html=True
-)
