d080c6447a6e6a787775f04d5e8b05e9dd5f2017
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-11 15:58:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -576,12 +576,11 @@ with tab5:
         'Lake': ln_df,
         'Winston': ws_df
     }
-    with st.container():
-        grid([plot_metric(dataframes, 'Sold Roll 90', 'Sales Trends Over the Last 90 Days', 'Sold Roll 90'),
-              plot_metric(dataframes, 'Sold-MTD', 'Month-to-Date Sales Performance', 'Sold-MTD')],
-             columns=2)
+    col1, col2 = st.columns(2)
+    with col1:
+        plot_metric(dataframes, 'Sold Roll 90', 'Sales Trends Over the Last 90 Days', 'Sold Roll 90')
+        plot_metric(dataframes, 'Sold-MTD', 'Month-to-Date Sales Performance', 'Sold-MTD')
     
-    with st.container():
-        grid([plot_metric(dataframes, 'Days Supply', 'Inventory Levels (Days Supply)', 'Days Supply'),
-              plot_metric(dataframes, 'Dlr Inventory', 'Dealer Inventory Comparison', 'Dlr Inventory')],
-             columns=2)
+    with col2:
+        plot_metric(dataframes, 'Days Supply', 'Inventory Levels (Days Supply)', 'Days Supply')
+        plot_metric(dataframes, 'Dlr Inventory', 'Dealer Inventory Comparison', 'Dlr Inventory')
